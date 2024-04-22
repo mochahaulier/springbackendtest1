404abf4cd4e083aba77b1c9222b21ae74d2d5ffe
mochahaulier <mochahaulier@gmail.com>
2024-04-22 22:15:19 +0200

    Changed to InputStreamReader.

--- src/main/java/dev/mochahaulier/springbackendtest1/services/DictionaryService.java
+++ src/main/java/dev/mochahaulier/springbackendtest1/services/DictionaryService.java
@@ -1,29 +1,55 @@
 package dev.mochahaulier.springbackendtest1.services;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.PostConstruct;
 
-@Service("ds")
+@Service
 public class DictionaryService {
 
-    private List<String> hiddenWords;
-    private List<String> validWords;
+    private List<String> hiddenWords = new ArrayList<>();
+    private List<String> validWords = new ArrayList<>();
 
     @PostConstruct
-    private void loadHiddenWords() throws IOException {
-        hiddenWords = FileUtils.readLines(new File("src/main/resources/hiddenwords.txt"), "utf-8");
+    private void loadHiddenWords() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    this.getClass().getResourceAsStream("/hiddenwords.txt"), "UTF-8"));
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                hiddenWords.add(line);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @PostConstruct
-    private void loadValidWords() throws IOException {
-        validWords = FileUtils.readLines(new File("src/main/resources/validwords.txt"), "utf-8");
+    private void loadValidWords() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    this.getClass().getResourceAsStream("/validwords.txt"), "UTF-8"));
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                hiddenWords.add(line);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getSolution() {
